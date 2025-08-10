8c2c062c49232e951ab1446755f169dae99590d1
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-08-10 10:29:08 +0800

    2025年8月10日

---
+++ pipe/2_运行.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+#!使用前请一定配置好ctl文件!!!!
+#!使用前请一定配置好ctl文件!!!!
+#!使用前请一定配置好ctl文件!!!!
+# 定义临时目录
+
+BASEML_PATH="/mnt/f/OneDrive/文档（科研）/脚本/Download/12-PAML/bin/baseml"
+CTL_FILE="/mnt/f/OneDrive/文档（科研）/脚本/Download/12-PAML/conf/baseml2.ctl"
+OUT_DIR="/mnt/f/OneDrive/文档（科研）/脚本/Download/12-PAML/output"
+
+
+# 拷贝所需ctl文件
+cp "$CTL_FILE" \
+    "$OUT_DIR/"
+
+# 切换到输出目录
+cd "$OUT_DIR"
+
+# 运行PAML baseml
+"${BASEML_PATH}" \
+    "${CTL_FILE}"
+
+rm "${OUT_DIR}/baseml2.ctl" \
+   "${OUT_DIR}/lnf" \
+   "${OUT_DIR}/rst" \
+   "${OUT_DIR}/rst1" \
+   "${OUT_DIR}/rst2" \
+   "${OUT_DIR}/rub" \
+   "${OUT_DIR}/2base.t"
